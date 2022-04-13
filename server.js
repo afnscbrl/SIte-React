a3b86522d6dc373462aca5fd308414863f6150ba
afnscbrl <afns.cbrl@gmail.com>
2022-04-13 12:17:18 -0300

    fix error

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use('/', express.static(
     )
 ))
 
-app.listen(process.env .PORT || 3000, () => {
+app.listen(process.env .PORT || 3000, (err) => {
     if (err) {
         console.log(err);
     }
